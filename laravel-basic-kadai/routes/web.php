013120e250c53fe2ba7c842e213291999e68c38a
koma-tsuna <hidari.kiki.1717@gmail.com>
2024-11-08 05:43:54 +0900

    laravel-basic-kadai-2_1回目

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// ルーティングを設定するコントローラーの宣言
+use App\Http\Controllers\PostController;
+use PHPUnit\Framework\Attributes\PostCondition;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index']);
