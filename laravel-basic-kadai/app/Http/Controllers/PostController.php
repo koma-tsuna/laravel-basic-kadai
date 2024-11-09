60d6fe17d87c99a18b79f11b09121981de87c7af
koma-tsuna <hidari.kiki.1717@gmail.com>
2024-11-09 11:14:08 +0900

    laravel-basic-kadai-3_1回目

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // postsテーブルからすべてのデータを取得し、$postsに代入する
+        $posts = DB::table('posts')->get();
+
+        // 変数$postsをpost/index.blade.phpファイルにわたす
+        return view('posts.index', compact('posts'));
     }
 
 }
--- laravel-basic-kadai/resources/views/posts/index.blade.php
+++ laravel-basic-kadai/resources/views/posts/index.blade.php
@@ -3,12 +3,26 @@
 
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width", intial-scale=1.0">
+        <meta name="viewport" content="width=device-width", intial-scale="1.0">
         <title>Laravel基礎</title>
     </head>
 
     <body>
         <h1>投稿一覧</h1>
+
+        <table border="1" style="border-collapse: collapse; border-color:blue";>
+            <tr>
+                <th>タイトル</th>
+                <th>本文</th>
+            </tr>
+            @foreach($posts as $post)
+                <tr>
+                    <td>{{ $post->title}}</td>
+                    <td>{{ $post->content}}</td>                
+                </tr>
+            @endforeach
+
+        </table>
     </body>
     
 </html>
